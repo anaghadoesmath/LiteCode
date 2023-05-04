cc9b322dcedd3d9349e6aa8b93648c6a400a1052
Anagha Gokul <99983935+anaghadoesmath@users.noreply.github.com>
2023-05-04 07:46:11 +0530

    Create 649. Dota2 Senate.cpp
    Fixed issues with same index

---
+++ Medium/649. Dota2 Senate.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        int n= senate.length();
+
+        queue<int> radiant, dire;
+        for(int i=0; i<n; i++)
+        {
+            if(senate[i]=='R')
+            {
+                radiant.push(i);
+            }
+            else
+            {
+                dire.push(i);
+            }
+        }
+        while(!radiant.empty()&& !dire.empty())
+        {
+            int r= radiant.front();
+            int d= dire.front();
+            radiant.pop();
+            dire.pop();
+            if(r < d)
+            {
+                radiant.push(r + n);
+            }
+            else
+            {
+                dire.push(d + n);
+            }
+        }
+
+            
+            
+        
+        if(radiant.size()<dire.size())
+        {
+            return "Dire";
+        }
+        else
+        {
+            return "Radiant";
+        }
+        
+    }
+};
